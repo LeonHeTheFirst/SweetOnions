ee51787bf692fd924280d4fe1639cf0c53884e7f
Austin Chu <austinchu@wirelessprvnat-172-17-71-217.near.illinois.edu>
2016-04-01 14:54:56 -0500

    added print statements

--- directory.py
+++ directory.py
@@ -17,18 +17,21 @@ serversocket.listen(5)
 #get the public keys from the nodes and store them
 int x = 0
 while 1:
+    print "Ready to accept connections\n"
     #accept connections from outside
     connection, address = serversocket.accept()
 
     #data recieved from connection
     data = connection.recv[1024]
-
+    print "Data recieved\n"
     #if connection is from client, send IPs and public keys
     if data == "client":
+        print "Connection to Client, sending IPs and public keys"
     	toBeSent = ', '.join(i + ', ' + j for i,j in zip(ipList,publicKeyList))
     		connection.sendall(toBeSent)
 
     #else it is a node connection, store public key sent from node
     else:
+        print "Connection to node, storing public key"
 	    publicKeyList.append(connection.recv[1024])
 	    x += 1
