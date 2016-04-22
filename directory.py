d380f2b28c01d6f682c1c14ec4600e047be92881
Shivam Khanna <shivamkhanna@wirelessprvnat-172-17-121-243.near.illinois.edu>
2016-04-21 21:50:58 -0500

    Changed Directory.py (Shivam)

--- directory.py
+++ directory.py
@@ -4,17 +4,16 @@
 # their information (i.e. changing public keys, etc.).
 
 import socket
+import random
 
-NUM_ENTRY = 2
-NUM_EXIT = 2
 NUM_ROUTERS = 5
 
-entryRoutersDict = {}
-exitRoutersDict = {}
+routerCount = 0
 onionRoutersDict = {}
+randomSelection = []
 
 directoryServer = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-directoryServer.bind((socket.gethostname(), 80))
+directoryServer.bind((socket.gethostname(), 1600))
 directoryServer.listen(5)
 
 # Begin listening
@@ -22,27 +21,23 @@ while 1:
 	myClientSocket, myClientAddress = directoryServer.accept()
 	dataReceived = myClientSocket.recv(512)
 	
-	# Initialization: Communicate with all onion routers until all keys are 
-	# stored.	
+	# Initialization: Communicate with all onion routers until all keys are stored.	
 	if dataReceived == "Onion Router":
-		onionRoutersDict[myClientAddress] = dataReceived
+		onionRoutersDict[routerCount] = myClientAddress + ", " + dataReceived
+		routerCount = routerCount + 1
 		print "Onion Router Information Received [" + myClientAddress + "] - [" + dataReceived + "]"
-	else if dataReceived == "Entry Router":
-		entryRoutersDict[myClientAddress] = dataReceived
-		print "Entry Router Information Received [" + myClientAddress + "] - [" + dataReceived + "]"
-	else if dataReceived == "Exit Router":
-		exitRoutersDict[myClientAddress] = dataReceived
-		print "Exit Router Information Received [" + myClientAddress + "] - [" + dataReceived + "]"
 
 	# Initialization complete. 
-	if dataReceived == "Client Request" and len(onionRoutersDict) == NUM_ROUTERS and len(entryRoutersDict) == NUM_ENTRY and len(exitRoutersDict) == NUM_EXIT:
-		print "Client Request"
-		# Send client data
+	if dataReceived == "Client Request" and len(onionRoutersDict) == NUM_ROUTERS:
 
+		# Select random routers to choose from.
+		while len(randomSelection) != 3:
+			randomNum = random.randrange(0, NUM_ROUTERS)
+			if randomNum not in randomSelection:
+				randomSelection.append(randomNum)
 
+		# Develop and send message.
+		message = onionRoutersDict[randomSelection[0]] + ", " + onionRoutersDict[randomSelection[1]] + ", " + onionRoutersDict[randomSelection[2]]
+		myClientSocket.send(message)
 
-
-
-
-
-
+		randomSelection = []
