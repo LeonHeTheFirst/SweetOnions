abce416282b466bfd662ecaff26f2928134af804
Leon He <leonhe2@illinois.edu>
2016-05-03 16:16:22 -0500

    cleaned imports

--- aes_rsa.py
+++ aes_rsa.py
@@ -1,15 +1,9 @@
 # This file contains functions used for multi-layered encryption
-import sys
 import os
-from os import chmod
-import socket
+import base64
 import Crypto
 from Crypto.PublicKey import RSA
-from Crypto import Random
 from Crypto.Cipher import AES
-import base64
-import random
-import hashlib
 
 BLOCK_SIZE = 16
 PADDING = '{'
