a2eaa45bcf5ddbe4bd04758f749a2f64119eff32
Shivam Khanna <shivamkhanna@wirelessprvnat-172-17-165-170.near.illinois.edu>
2016-05-02 21:58:41 -0500

    Node.py Bug Fixed

--- node.py
+++ node.py
@@ -69,7 +69,7 @@ addr = addr[0]
 data = conn.recv(BUFFER_SIZE)
 #decrypted = privRSAkey.decrypt(data)
 dataArr = data.split(",")
-for x in range(5):
+for x in range(1):
     NODES[dataArr[2 * x]] = dataArr[2 * x + 1]
 conn.close()
 s.close()        
