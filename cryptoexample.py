766345bf6fb1db9d03c0b499aa5f463f9745fb51
Leon He <leonhe2@illinois.edu>
2016-05-02 23:48:11 -0500

    RSA/AES example

--- cryptoexample.py
+++ cryptoexample.py
@@ -1,15 +1,49 @@
 
 import sys
+import os
 from os import chmod
 import socket
 import Crypto
 from Crypto.PublicKey import RSA
 from Crypto import Random
+from Crypto.Cipher import AES
+import base64
 import ast
 import random
 import hashlib
 import random
 
+msg = '''
+attack at dawn
+attack at dawn
+attack at dawn
+attack at dawn
+attack at dawn
+attack at dawn
+attack at dawn
+attack at dawn
+attack at dawn
+attack at dawn
+attack at dawn
+attack at dawn
+attack at dawn
+attack at dawn
+attack at dawn
+
+'''
+
+BLOCK_SIZE = 16
+PADDING = '{'
+pad = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * PADDING
+EncodeAES = lambda c, s: base64.b64encode(c.encrypt(pad(s)))
+DecodeAES = lambda c, e: c.decrypt(base64.b64decode(e)).rstrip(PADDING)
+#generate the random key
+secret = os.urandom(BLOCK_SIZE)
+print "secret: ", base64.b64encode(secret)
+#create the encrypter
+cipher = AES.new(secret)
+#encrypt
+encrpytedMsg = EncodeAES(cipher, msg)
 
 key = RSA.generate(2048)
 
@@ -19,10 +53,17 @@ binPubKey =  key.publickey().exportKey('PEM')
 privKeyObj = RSA.importKey(binPrivKey)
 pubKeyObj =  RSA.importKey(binPubKey)
 
-msg = "attack at dawn"
-emsg = pubKeyObj.encrypt(msg, 32)[0]
-print emsg
-dmsg = privKeyObj.decrypt(emsg)
-print dmsg
 
-assert(msg == dmsg)
+eKey = pubKeyObj.encrypt(base64.b64encode(secret), 32)[0]
+print eKey
+#decrypt the key
+dKey = privKeyObj.decrypt(eKey)
+print dKey
+
+#Use decrypted AES key to decrypt encrypted message
+decrypter = AES.new(base64.b64decode(dKey))
+decryptedMsg = DecodeAES(decrypter, encrpytedMsg)
+print decryptedMsg
+
+
+
